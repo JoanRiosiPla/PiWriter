70647fd5eb25ca361f061e68ae5704c586c80401
Joan Rios i Pla <joanriosipla@gmail.com>
2023-01-30 15:02:26 +0100

    Basic things

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 // Joan Rios i Pla
 #include <iostream>
-#include <functions.cpp>
+#include <fstream>
+//#include <functions.cpp>
 using namespace std;
 
 int main(void)
 {
-    
+    fstream adjectives;
+    adjectives.open("words/adjectives.csv",ios::out);
+    if(!adjectives) 
+    {
+        cout<<"File creation failed";
+    }
+    else
+    {
+        cout<<"New file created";
+        adjectives.close(); // Step 4: Closing file
+    }
+    return 0;
 }
