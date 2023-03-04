cf96d66abdee63fd578b9696b103c99990004d43
Joan Rios i Pla <joanriosipla@gmail.com>
2023-03-04 21:57:29 +0100

    Added loading files, getW...ndLen, updated things

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
-// Joan Rios i Pla
+// Developed by Joan Rios i Pla.
+// It's not possible to make this without human imput, we will persent the human with possible options.
+
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <cmath>
 #include <string>
 #include <vector>
-//#include "functions.hpp"
+#include <bits/stdc++.h>
+// #include "functions.hpp"
 
 using namespace std;
 
@@ -14,14 +17,17 @@ typedef struct
     string text = "[]";
     string type = "[]";
     int length = 0;
-}word;
+} word;
 
 word options[9];
 word sentence[100];
 
-// TODO
-vector<word> words[8];
+const int numberOfWordFiles = 10;
+string typesOfWords[numberOfWordFiles] = {"noun", "pronoun", "verb", "adjective", "adverb", "preposition",
+                                          "determiner", "conjunction", "interjection", "pi"};
+vector<word> words[numberOfWordFiles];
 string pi;
+int current = 0;
 
 void updateOptions();
 string endOfSentence();
@@ -31,22 +37,86 @@ bool addToOptions(word addition);
 int lengthOfSentence();
 int lengthOfOptions();
 bool addToSentence(word input);
+vector<word> getWordsOfTypeAndLen(int type, int len);
 
 int main(void)
 {
+
+    // Start rand()
+    srand((unsigned) time(NULL));
+
     flushOptions();
     flushSentence();
-    
+
+    // Load files into vectors.
+
+    {
+        int time_before_load = time(NULL);
+
+        for (int i = 0; i < numberOfWordFiles; i++)
+        {
+            fstream tmp;
+            tmp.open("words/" + typesOfWords[i] + ".csv", ios::in);
+            if (!tmp)
+            {
+                cout << "File not found: " << typesOfWords[i] << endl
+                     << "Attempting creation of file: " << typesOfWords[i] << endl;
+                tmp.open("words/" + typesOfWords[i] + ".csv", ios::out | ios::in | ios::app);
+                if (tmp)
+                {
+                    cout << "Creation succesful";
+                }
+                else
+                {
+                    cout << "Failed.";
+                    return 1;
+                }
+            }
+            else
+            {
+                cout << "Opened " << typesOfWords[i];
+            }
+            cout << endl;
+
+            // Load into vector;
+            cout << "Loading file into vector ... ";
+            if (typesOfWords[i] != "pi")
+            {
+                while (!tmp.eof())
+                {
+                    string str;
+                    getline(tmp, str);
+                    word idk = {str, typesOfWords[i], str.length()};
+                    words[i].push_back(idk);
+                }
+            }
+            else
+            {
+                getline(tmp, pi);
+            }
+            
+            cout << "Should be loaded" << endl;
+
+            // Close file
+            tmp.close();
+        }
+
+        // TOFIX
+        int time_after_load = time(NULL);
+        int time_diff = time_after_load - time_before_load;
+        cout << "Loaded in " << time_diff << " seconds." << endl;
+    }
+
     string input;
     while (true)
     {
         cout << endl
-        <<"End of current text:" << endOfSentence() << endl
-        << "Chose one of the following:" << endl;
+             << "End of current text:" << endOfSentence() << endl
+             << "Chose one of the following:" << endl;
         updateOptions();
-        for (int i = 0; i < lengthOfOptions() ; i++)
+        for (int i = 0; i < lengthOfOptions(); i++)
         {
-            cout << i << ": " << options[i].text;
+            cout << i << ": " << options[i].text << endl;
         }
         cout << endl;
         cin >> input;
@@ -54,29 +124,51 @@ int main(void)
     }
 }
 
-// It's not possible to make this without human imput, we will persent the human with possible options.
 
 void updateOptions()
 {
     flushOptions();
-    string type = sentence[sizeof(sentence)/sizeof(*sentence) - 1].type;
-    addToOptions({"hi", "interjection", 2});
+    string type = sentence[lengthOfSentence() - 1].type;
+    int chars = pi.at(current) - '0';
+
+    for (int i = 0; i < numberOfWordFiles - 1; i++)
+    {
+        vector<word> all = getWordsOfTypeAndLen(i, chars);
+        if (all.size() >= 3)
+        {
+            for(int i = 0; i < 3; i++) addToOptions(all[rand() % all.size()]);
+        }
+        else if (all.size() == 2)
+        {
+            for(int i = 0; i < 2; i++) addToOptions(all[rand() % all.size()]);
+        }
+        else if (all.size() == 1)
+        {
+            for(int i = 0; i < 1; i++) addToOptions(all[rand() % all.size()]);
+        }
+    }
+    current++;
 }
 
 string endOfSentence()
 {
     string out = "";
-    if (lengthOfSentence() >= 5) out += "... ";
-    if (lengthOfSentence() >= 4) out += sentence[lengthOfSentence() - 4].text + " ";
-    if (lengthOfSentence() >= 3) out += sentence[lengthOfSentence() - 3].text + " ";
-    if (lengthOfSentence() >= 2) out += sentence[lengthOfSentence() - 2].text + " ";
-    if (lengthOfSentence() >= 1) out += sentence[lengthOfSentence() - 1].text;
+    if (lengthOfSentence() >= 5)
+        out += "... ";
+    if (lengthOfSentence() >= 4)
+        out += sentence[lengthOfSentence() - 4].text + " ";
+    if (lengthOfSentence() >= 3)
+        out += sentence[lengthOfSentence() - 3].text + " ";
+    if (lengthOfSentence() >= 2)
+        out += sentence[lengthOfSentence() - 2].text + " ";
+    if (lengthOfSentence() >= 1)
+        out += sentence[lengthOfSentence() - 1].text;
     return out;
 }
 
 void flushOptions()
 {
-    for (int i = 0; i < sizeof(options)/sizeof(*options); i++)
+    for (int i = 0; i < sizeof(options) / sizeof(*options); i++)
     {
         options[i] = {"[empty]", "[none]", 0};
     }
@@ -85,7 +177,7 @@ void flushOptions()
 
 void flushSentence()
 {
-    for (int i = 0; i < sizeof(sentence)/sizeof(*sentence); i++)
+    for (int i = 0; i < sizeof(sentence) / sizeof(*sentence); i++)
     {
         sentence[i] = {"[empty]", "[none]", 0};
     }
@@ -94,7 +186,7 @@ void flushSentence()
 
 bool addToOptions(word addition)
 {
-    for (int i = 0; i < sizeof(options)/sizeof(*options); i++)
+    for (int i = 0; i < sizeof(options) / sizeof(*options); i++)
     {
         if (options[i].text.at(0) == '[')
         {
@@ -102,13 +194,13 @@ bool addToOptions(word addition)
             return true;
         }
     }
-    
+
     return false;
 }
 
 bool addToSentence(word addition)
 {
-    for (int i = 0; i < sizeof(sentence)/sizeof(*sentence); i++)
+    for (int i = 0; i < sizeof(sentence) / sizeof(*sentence); i++)
     {
         if (sentence[i].text.at(0) == '[')
         {
@@ -116,15 +208,14 @@ bool addToSentence(word addition)
             return true;
         }
     }
-    
+
     return false;
 }
-
 
 int lengthOfSentence()
 {
     int length = 0;
-    for (int i = 0; i < sizeof(sentence)/sizeof(*sentence); i++)
+    for (int i = 0; i < sizeof(sentence) / sizeof(*sentence); i++)
     {
         if (sentence[i].text.at(0) == '[')
         {
@@ -138,7 +229,7 @@ int lengthOfSentence()
 int lengthOfOptions()
 {
     int length = 0;
-    for (int i = 0; i < sizeof(options)/sizeof(*options); i++)
+    for (int i = 0; i < sizeof(options) / sizeof(*options); i++)
     {
         if (options[i].text.at(0) == '[')
         {
@@ -147,4 +238,17 @@ int lengthOfOptions()
         length++;
     }
     return length;
+}
+
+vector<word> getWordsOfTypeAndLen(int type, int len)
+{
+    vector<word> out;
+    for (auto i = words[type].begin(); i != words[type].end(); ++i)
+    {
+        if ((*i).length == len)
+        {
+            out.push_back(*i);
+        }
+    }
+    return out;
 }
